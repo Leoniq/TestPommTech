ced626d3628946e8c4ad78abdedfffd8042b15f5
Leoniq <64737619+Leoniq@users.noreply.github.com>
2021-09-29 16:26:45 +0300

    Update ExceptionController.php

--- src/Controller/ExceptionController.php
+++ src/Controller/ExceptionController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Repository\PostsRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
